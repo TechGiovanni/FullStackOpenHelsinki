251eab9ce42df3b48e5f8a4e1af44e36469219cb
Giovanni Reid <giovanni-mtl@hotmail.com>
2022-12-12 20:32:31 -0500

    restructured the parts data into an array of objects

--- Part1/src/App.js
+++ Part1/src/App.js
@@ -6,20 +6,29 @@ import Total from "./components/Total";
 
 const App = () => {
 	const course = "Half Stack application development";
-	const part1 = "Fundamentals of React";
-	const exercises1 = 10;
-	const part2 = "Using props to pass data";
-	const exercises2 = 7;
-	const part3 = "State of a component";
 	const exercises3 = 14;
+	const parts = [
+		{
+			name: "Fundamentals of React",
+			exercises: 10,
+		},
+		{
+			name: "Using props to pass data",
+			exercises: 7,
+		},
+		{
+			name: "State of a component",
+			exercises: 14,
+		},
+	];
 
-	const parts = [part1, exercises1, part2, exercises2, part3, exercises3];
+	// const parts = [part1, part2, part3];
 
 	return (
 		<div>
 			<Header course={course} />
 			<Content parts={parts} />
-			<Total total={exercises1 + exercises2 + exercises3} />
+			<Total parts={parts} />
 		</div>
 	);
 };
