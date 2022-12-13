1ab3a7913d69b99e6d5e1ea260f23a2dbdf2cac8
Giovanni Reid <giovanni-mtl@hotmail.com>
2022-12-13 17:34:23 -0500

    was able to style the buttons and display the texts from an array

---
+++ part-1/anecdotes/src/App.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { useState } from "react";
+import Button from "./components/Button";
+
+import "./App.css";
+
+const App = () => {
+	const [selected, setSelected] = useState(0);
+	const [anecdoteHistory, setAnecdoteHistory] = useState([0]);
+	const [vote, setVote] = useState(0);
+	const anecdotes = [
+		{
+			text: "If it hurts, do it more often.",
+			vote: 0,
+		},
+		{
+			text: "Adding manpower to a late software project makes it later!",
+			vote: 0,
+		},
+		{
+			text:
+				"The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+			vote: 0,
+		},
+		{
+			text:
+				"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+			vote: 0,
+		},
+		{
+			text: "Premature optimization is the root of all evil.",
+			vote: 0,
+		},
+		{
+			text:
+				"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+			vote: 0,
+		},
+		{
+			text:
+				"Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+			vote: 0,
+		},
+	];
+
+	return (
+		<>
+			<div className="wrapper">
+				<div className="content">
+					<div class="content__text">{anecdotes[selected]}</div>
+					<div>
+						<Vote />
+						<Button
+							anecdotes={anecdotes}
+							selected={selected}
+							setSelected={setSelected}
+							setAnecdoteHistory={setAnecdoteHistory}
+							anecdoteHistory={anecdoteHistory}
+						>
+							Next Anecdote
+						</Button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default App;
---
+++ part-1/anecdotes/src/components/Button.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import "./Button.css";
+
+const Button = (props) => {
+	//
+	const RandAnecdotesHandler = (props) => {
+		const random = () => {
+			const max = props.anecdotes.length;
+			let rand = Math.floor(Math.random() * max);
+
+			// keeps track of each random number generated
+			props.setAnecdoteHistory(props.anecdoteHistory.concat(rand));
+			console.log(props.anecdoteHistory);
+
+			// sets the random number
+			return props.setSelected(rand);
+		};
+
+		return random;
+	};
+
+	return (
+		<div>
+			<button class="btn" onClick={RandAnecdotesHandler(props)}>
+				{props.children}
+			</button>
+		</div>
+	);
+};
+
+export default Button;
