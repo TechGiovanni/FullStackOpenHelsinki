6bc6675158dc34e5674037304ac89bc2e269f928
Giovanni Reid <giovanni-mtl@hotmail.com>
2022-12-12 21:19:20 -0500

    fixed all bugs for now, was able to retreive data from an object of different data structures inside

--- Part1/src/components/Total.jsx
+++ Part1/src/components/Total.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 function Total(props) {
-	// console.log(props.parts);
+	// console.log(props.parts.parts);
+	const { parts } = props.parts;
 	return (
 		<div>
 			<p>
-				{/* Number of exercises{" "}
-				{props.parts[0].exercises +
-					props.parts[1].exercises +
-					props.parts[2].exercises} */}
+				Number of exercises{" "}
+				{parts[0].exercises + parts[1].exercises + parts[2].exercises}
 			</p>
 		</div>
 	);
