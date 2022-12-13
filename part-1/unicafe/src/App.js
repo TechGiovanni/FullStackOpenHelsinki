e9185fa9ce494c27a7de69301c0d90264a1e1224
Giovanni Reid <giovanni-mtl@hotmail.com>
2022-12-13 01:11:30 -0500

    implemented the state and onClick functions

--- part-1/unicafe/src/App.js
+++ part-1/unicafe/src/App.js
@@ -2,16 +2,27 @@ import "./App.css";
 import React, { useState } from "react";
 
 const App = () => {
+	// save clicks of each button to its own state
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
 
+	const increaseGood = () => {
+		setGood(good + 1);
+	};
+	const increaseNeutral = () => {
+		setNeutral(neutral + 1);
+	};
+	const increaseBad = () => {
+		setBad(bad + 1);
+	};
+
 	return (
 		<>
 			<h1>Give Feedback</h1>
-			<button>Good</button>
-			<button>Neutral</button>
-			<button>Bad</button>
+			<button onClick={increaseGood}>Good</button>
+			<button onClick={increaseNeutral}>Neutral</button>
+			<button onClick={increaseBad}>Bad</button>
 
 			<h2>Statistics</h2>
 			<p>Good {good}</p>
