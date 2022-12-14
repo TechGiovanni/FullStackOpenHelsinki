d2a804697333673701dc93d2d4743133cdaf33e3
Giovanni Reid <giovanni-mtl@hotmail.com>
2022-12-14 14:35:19 -0500

    was able to display ccontent from user input and prevent default refresh

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -2,22 +2,51 @@ import React from "react";
 import { useState } from "react";
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
 	const [newName, setNewName] = useState("");
+	const [persons, setPersons] = useState([
+		{ name: "Arto Hellas", id: 1 },
+		{ name: "Johnson", id: 2 },
+	]);
+
+	// const man = {};
+
+	const newNameHandler = (event) => {
+		console.log("Connected to input");
+		console.log("Input text:", event.target.value);
+		setNewName(event.target.value);
+	};
+
+	const addNewPersonHandler = (event) => {
+		event.preventDefault();
+		//
+		const newPersonObject = {
+			name: newName,
+			id: persons.length + 1,
+		};
+		//
+		console.log("newperson", newPersonObject);
+		setPersons(persons.concat(newPersonObject));
+		setNewName("");
+	};
+
+	console.log("Person", persons);
 
 	return (
 		<div>
+			<div>debug: {newName}</div>
 			<h2>Phonebook</h2>
-			<form>
+			<form onSubmit={addNewPersonHandler}>
 				<div>
-					name: <input />
+					name: <input value={newName} onChange={newNameHandler} />
 				</div>
 				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			...
+			{persons.map((v) => {
+				return <p key={v.id}>{v.name}</p>;
+			})}
 		</div>
 	);
 };
